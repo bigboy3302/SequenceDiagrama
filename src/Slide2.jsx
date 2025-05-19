87e7ad37bbd8bfed53906e2718f90c13e3316f2e
Adrians Raitums <126756100+bigboy3302@users.noreply.github.com>
2025-05-19 12:48:47 +0300

    Update Slide2.jsx

--- src/Slide2.jsx
+++ src/Slide2.jsx
@@ -3,8 +3,16 @@ export default function Slide2() {
     <article>
       <h1>Kad to izmanto?</h1>
       <p>
-        To izmanto, lai attēlotu, kā objekti sazinās un apmainās ar ziņām laikā.
+        Sekvences diagrammas ir daļa no UML, un tās tiek izmantotas, lai attēlotu objektu savstarpējo mijiedarbību noteiktā laika secībā.
       </p>
+      <br>
+        <p>Sistēmas vai funkcijas darbības secības modelēšana.
+
+Procesu simulācija, piemēram, lietotāja pieprasījuma apstrāde.
+
+Sistēmas komponentu sadarbības analīze.
+
+Komunikācijas dizains starp objektiem programmēšanā vai sistēmu izstrādē.</p>
     </article>
   );
 }
